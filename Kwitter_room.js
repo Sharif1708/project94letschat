3699ecf75ee031a96993d293a3e088a1c14e0695
hud <93031562+Sharif1708@users.noreply.github.com>
2023-08-17 15:30:10 +0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+		apiKey: "AIzaSyDlH7mSBmHfWZVDpf18cL4-QpunbpW4cJA",
+    authDomain: "project93-96-cd9f3.firebaseapp.com",
+    databaseURL: "https://project93-96-cd9f3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "project93-96-cd9f3",
+    storageBucket: "project93-96-cd9f3.appspot.com",
+    messagingSenderId: "815160784273",
+    appId: "1:815160784273:web:46f70a4def5747f5675630"
+	};
+
+	firebase.initializeApp(firebaseConfig);
